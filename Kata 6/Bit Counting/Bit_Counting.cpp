3c435b3f280ce837b9cd350254b85c0453a41c76
AshenOne17 <artem08.mail.com@gmail.com>
2022-12-17 20:16:03 -0500

    Updated repository. Added some 6 Katas'.

---
+++ Kata 6/Bit Counting/Bit_Counting.cpp
@@ -0,0 +1,23 @@
+// Write a function that takes an integer as input, and returns the number of bits that are equal to one in the binary representation of that number.
+// You can guarantee that input is non - negative.
+//
+// Example: The binary representation of 1234 is 10011010010, so the function should return 5 in this case.
+
+unsigned int countBits(unsigned long long n)
+{
+    if (n == 0)
+        return 0;
+
+    int bitCount = 0;
+
+    while (n > 0)
+    {
+        int bit = n % 2;
+        n /= 2;
+
+        if (bit == 1) 
+            bitCount++;
+    }
+
+    return bitCount;
+}
