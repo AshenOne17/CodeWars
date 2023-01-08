fead0791a6b9b67ebc4b0a054975d39b73b3bed0
AshenOne17 <artem08.mail.com@gmail.com>
2023-01-07 23:25:17 -0500

    Updated repository. Added some 6 Katas'.

---
+++ Kata 6/Are they the same/Are_they_the_same.cpp
@@ -0,0 +1,66 @@
+/*
+* 
+ Given two arrays a and b write a function comp (a, b) (or Same::comp(a, b)) that checks whether the two arrays have the "same" 
+ elements, with the same multiplicities(the multiplicity of a member is the number of times it appears). 
+ "Same" means, here, that the elements in b are the elements in a squared, regardless of the order.
+
+ Examples:
+ Valid arrays:
+ a = [121, 144, 19, 161, 19, 144, 19, 11]
+ b = [121, 14641, 20736, 361, 25921, 361, 20736, 361]
+
+ comp(a, b) returns true because in b 121 is the square of 11, 14641 is the square of 121, 20736 the square of 144, 361 the square of 19,
+ 25921 the square of 161, and so on. It gets obvious if we write b's elements in terms of squares:
+ a = [121, 144, 19, 161, 19, 144, 19, 11]
+ b = [11 * 11, 121 * 121, 144 * 144, 19 * 19, 161 * 161, 19 * 19, 144 * 144, 19 * 19]
+
+ Invalid arrays:
+ If, for example, we change the first number to something else, comp is not returning true anymore:
+ a = [121, 144, 19, 161, 19, 144, 19, 11]
+ b = [132, 14641, 20736, 361, 25921, 361, 20736, 361]
+
+ comp(a, b) returns false because in b 132 is not the square of any number of a.
+ a = [121, 144, 19, 161, 19, 144, 19, 11]
+ b = [121, 14641, 20736, 36100, 25921, 361, 20736, 361]
+
+ comp(a, b) returns false because in b 36100 is not the square of any number of a.
+
+ Remarks
+ a or b might be[] or {} (all languages except R, Shell).
+ a or b might be nil or null or None or nothing(except in C++, COBOL, Crystal, D, Dart, Elixir, Fortran, F#, Haskell, Nim, OCaml, Pascal, Perl, PowerShell, Prolog, PureScript, R, Racket, Rust, Shell, Swift).
+ If a or b are nil(or null or None, depending on the language), the problem doesn't make sense so return false.
+
+ Note for C
+ The two arrays have the same size(> 0) given as parameter in function comp.
+* 
+*/
+
+#include <vector>
+#include <map>
+
+class Same {
+public:
+    static bool comp(std::vector<int>& a, std::vector<int>& b) {
+        // Initialize a map to store the squares of the elements of a and the number of times each square appears.
+        std::map<int, int> squares;
+
+        for (int i = 0; i < a.size(); i++) 
+        {
+            // Store the square of the current element of a in the map. 
+            // If the square is already present in the map, increment its count.
+            squares[a[i] * a[i]]++;
+        }
+        for (int i = 0; i < b.size(); i++) 
+        {
+            // Check if the current element of b is present in the map.
+            if (--squares[b[i]] < 0) {
+                // If the count for the element becomes negative, it means that
+                // an element of b is not the square of an element of a.
+                // In this case, return false.
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
